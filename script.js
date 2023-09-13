3f0cbe43f09a3165c5af6bd6cc6040cc5e0a450f
Blaise <jetblaise@gmail.com>
2023-09-13 14:12:48 -0400

    Gradient

--- script.js
+++ script.js
@@ -8,22 +8,16 @@ for (let i = 0; i < 64; i++) {
   const tile = document.createElement("div");
   tile.style.height = "12.5%";
   tile.style.width = "12.5%";
-  if (
-    (i >= 8 && i < 16) ||
-    (i >= 24 && i < 32) ||
-    (i >= 40 && i < 48) ||
-    (i >= 56 && i < 64)
-  ) {
-    if (i % 2 === 0) {
-      tile.style.background = "black";
-    } else {
-      tile.style.background = "red";
-    }
-  } else if (i % 2 === 0) {
-    tile.style.background = "red";
-  } else {
-    tile.style.background = "black";
-  }
+  tile.style.boxSizing = "border-box";
+
+  const row = Math.floor(i / 8);
+  const col = i % 8;
+
+  const red = Math.floor((row / 7) * 255);
+  const blue = Math.floor((col / 7) * 255);
+  tile.style.background = `rgb(${red}, 0, ${blue})`;
+  tile.style.border = "1px solid black";
+
   container.append(tile);
 }
 document.body.append(container);
