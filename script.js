0288c6881b0e7f65197e41dcb30129661489ee36
Blaise <jetblaise@gmail.com>
2023-09-13 14:22:10 -0400

    Flash every 2 seconds

--- script.js
+++ script.js
@@ -4,26 +4,28 @@ container.style.flexWrap = "wrap";
 container.style.width = "800px";
 container.style.height = "800px";
 
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+const tiles = [];
 for (let i = 0; i < 64; i++) {
   const tile = document.createElement("div");
   tile.style.height = "12.5%";
   tile.style.width = "12.5%";
-  if (
-    (i >= 8 && i < 16) ||
-    (i >= 24 && i < 32) ||
-    (i >= 40 && i < 48) ||
-    (i >= 56 && i < 64)
-  ) {
-    if (i % 2 === 0) {
-      tile.style.background = "black";
-    } else {
-      tile.style.background = "red";
-    }
-  } else if (i % 2 === 0) {
-    tile.style.background = "red";
-  } else {
-    tile.style.background = "black";
-  }
+  tiles.push(tile);
   container.append(tile);
 }
 document.body.append(container);
+
+function updateColors() {
+  for (let i = 0; i < tiles.length; i++) {
+    const tile = tiles[i];
+    tile.style.background = getRandomColor();
+  }
+}
+
+setInterval(updateColors, 2000);
